2aec7aeed805cf9d7f392caec581327505d126d7
BlueMcFlurry <57018533+BlueMcFlurry@users.noreply.github.com>
2021-03-05 14:48:15 -0600

    Added reducers and local storage functionality

--- booklist/src/components/BookForm.js
+++ booklist/src/components/BookForm.js
@@ -2,13 +2,13 @@ import React, { useContext, useState } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const NewBookForm = () => {
-    const { addBook } = useContext(BookContext);
+    const { dispatch } = useContext(BookContext);
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault();
-        addBook(title, author);
-        setTitle(''); 
+        dispatch({ type: 'ADD_BOOK', book: { title, author } });
+        setTitle('');
         setAuthor('');
     }
 
@@ -22,7 +22,7 @@ const NewBookForm = () => {
                 type='text' placeholder="author" required
                 value={author} onChange={(e) => setAuthor(e.target.value)}
             />
-            <input type='submit' value="add book"/>
+            <input type='submit' value="add book" />
         </form>
     );
 }
