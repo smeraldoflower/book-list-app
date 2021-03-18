0817b048f441787cf75ea550264bcf8c066b5b3c
BlueMcFlurry <57018533+BlueMcFlurry@users.noreply.github.com>
2021-03-18 00:49:36 -0500

    installed netlify dependencies

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BookListPage from './BookListPage';
+import AuthContextProvider from './contexts/AuthContext';
+import LoginPage from './LoginPage';
 
 function App() {
   return (
     <div className="App">
-        {/* <Router>
+      {/* <AuthContextProvider> */}
+        <Router>
           <Switch>
             <Route path='/' component={BookListPage} />
+            {/* <Route path='/' component={LoginPage} /> */}
           </Switch>
-        </Router> */}
-        <BookListPage/>
+        </Router>
+      {/* </AuthContextProvider> */}
     </div>
   );
 }
