b149aded357589c8f2a1edc4eb0302fcf475e20a
BlueMcFlurry <57018533+BlueMcFlurry@users.noreply.github.com>
2021-03-18 00:35:39 -0500

    deployment fixes

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BookListPage from './BookListPage';
-import AuthContextProvider from './contexts/AuthContext';
-import LoginPage from './LoginPage';
 
 function App() {
   return (
     <div className="App">
-      {/* <AuthContextProvider> */}
-        <Router>
+        {/* <Router>
           <Switch>
             <Route path='/' component={BookListPage} />
-            {/* <Route path='/' component={LoginPage} /> */}
           </Switch>
-        </Router>
-      {/* </AuthContextProvider> */}
+        </Router> */}
+        <BookListPage/>
     </div>
   );
 }
